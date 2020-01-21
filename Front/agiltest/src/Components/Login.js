aebccbb7155883753c5ee8b8bd5a06cabe757de5
Esoriak <solene@monsieurguiz.fr>
2020-01-21 01:25:41 +0100

    add function for calcul the score and display good styled button choices, add style tony, and page home

--- Front/agiltest/src/Components/Login.js
+++ Front/agiltest/src/Components/Login.js
@@ -1,13 +1,95 @@
 import React, { Component } from 'react'
 import {NavLink} from "react-router-dom"
+import './Style.css'
 
  class Login extends Component {
+
+
+// //******* CONNECTION *********//
+
+// emailCheck(val){
+   
+//   // EMAIL CHECKER
+//  /* if(val.indexOf("@")<=0){
+//      console.log("nope");
+//      }else{
+//          console.log("presque");
+//          if(val.indexOf(".")>=0 && val.indexOf(".")>val.indexOf("@")+1 && val.indexOf(".")<val.length-2){
+//              console.log ("GOOOOOOOOOOO");
+//          }
+//      }*/
+  
+//   // EMAIL EXTENSION CHECKER
+//   if(val.indexOf("cdc.com") >=4){
+//       $('.connect_button').addClass('input_button__active');
+//       //CONNECT OK
+//       $('.connect_button.input_button__active').click(function(){
+//           connectOK();
+//       })
+//      }else{
+//          $('.connect_button').removeClass('input_button__active');
+//      }
+// }
+
+// connectOK(){
+    
+//   // HIDE WELCOME CARD
+//   $('.welcome_card').animate({
+//       opacity: 0,
+//       left: "-=50",
+//     }, 250, function() {
+//       // Animation complete.
+//        $('.welcome_card').css("display","none");
+      
+//        //SHOW START CARD
+//       $('.start_card').css("display","flex");
+
+//       $('.start_card').animate({
+//           opacity: 1,
+//           left: "0",
+//         }, 250, function() {
+//           // Animation complete.
+//       });
+//   });
+  
+// }
+// //******* END-CONNECTION *********//
+
+
+
   render() {
     return (
       <div>
-        <h2> Coucou connecte toi</h2>
-        <NavLink path to="/quizz"><button > Se connecter</button></NavLink>
-      </div>
+       <div className="main_container">
+            <div className="card welcome_card">
+            <h1>Bienvenue sur notre plateforme d’entrainement à la certification PSPO !</h1>
+            <p>Veuillez entrer <b>votre adresse email</b> pour pouvoir accéder à notre questionnaire qui permettra de tester les connaissances requises afin d’obtenir la certificaiton PSPO.</p>
+            <div className="login-email">
+                <input type="email" className="input_email" placeholder="First name" onKeyUp="emailCheck(this.value)" />
+            </div>
+            
+            <div className="input_button input_button__inactive connect_button">Accéder à la plateforme</div>
+        </div>
+       <div className="card start_card">
+            <h1>Vous êtes connecté !</h1>
+            <p>Vous pouvez dès maintenant accéder à notre test d'entrainement à la certification PSPO</p>
+            
+           <div className="quizz_choices">
+               <p>Sélectionnez la durée du test :</p>
+               <div className="quizz_choice quiz_short" id="quizz_choice_20" onClick="chooseQuizz(20)" >15 min <br/><span className="minor_txt">(20 Questions)</span></div>
+               <div className="quizz_choice quiz_medium" id="quizz_choice_40" onClick="chooseQuizz(40)" >30 min <br/><span className="minor_txt">(40 Questions)</span></div>
+               <div className="quizz_choice quizlong"  id="quizz_choice_80" onClick="chooseQuizz(80)" >60 min <br/><span className="minor_txt">(80 Questions)</span></div>
+           </div>
+           
+           <NavLink path to="/quizz"><div className="input_button input_button__inactive start_button">Commencer le test</div></NavLink>
+        </div>
+        </div>
+        <footer>
+            <a href="www.monsieurguiz.fr" className="footer__byMG_link">
+                <img src="./imgs/byMG.svg" alt="by Monsieur Guiz" />
+            </a>
+        </footer>
+        </div>
     )
   }
 }
--- Front/agiltest/src/Components/Tests.js
+++ Front/agiltest/src/Components/Tests.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 import './Tests.css'
-import Buttontest from './Buttontest'
 
 const Questionnaire = []
 let questions =[]
-let choix = ''
+let choixUnique =0
+let choixMultiple =[]
+let score =0
 
  class Tests extends Component {
   state ={
@@ -15,6 +16,7 @@ let choix = ''
     questions: [],
     choices: [],
     choices_selected : [],
+    right_answers : 0,
     questionnaire : [],
     ready : false,
     displayQuestion : 1,
@@ -42,7 +44,7 @@ let choix = ''
 
  GetDataChoices = async() => {
    const choices = await axios.get('http://localhost:4000/quizzchoices/choices')
-  //  console.log('là les choix', choices.data)
+   console.log('là les choix', choices.data)
    this.setState({
      choices : choices.data
    })
@@ -51,73 +53,59 @@ let choix = ''
 
 
 RandomQuestionnaire = () => {
-    // Tant que le tableau n'a pas 80 nombre uniques on lance la requête
+    // Tant que le tableau n'a pas 80 nombres uniques on lance la requête
    while (questions.length < 80) {
-    // On récupère un nombre aléatoire compris entre 0 et 150 ( le nombre de questions exploitables pour le moment)
-   const random = Math.floor(Math.random() * 150)
-   // On copie le nombre dans le tableau des questions
-   questions.push(random+1)
+      // On récupère un nombre aléatoire compris entre 0 et 150 ( le nombre de questions exploitables pour le moment)
+      const random = Math.floor(Math.random() * 150)
+      // On copie le nombre dans le tableau des questions
+      questions.push(random+1)
     }   
-    // Si le tableau de nombre est egal a 80 on vérifie que toutes les valeurs sont uniques
+    // Si le tableau de nombre est egal à 80 on vérifie que toutes les valeurs sont uniques
    if (questions.length == 80) {
-    questions = [... new Set(questions)]
-   // Si le tableau de nombre uniques est égal à 80, on lance une boucle qui va remplir le questionnaire
-    if (questions.length == 80) {
-      console.log("la fin", questions)
-      console.log("chuck norris gagne toujours")
-      for (let i=0; i< questions.length; i++) {
-        // On cherche l'id de la question correspondant au chiffre random dans notre base de données et on la copie dans le tableau du questionnaire
-        Questionnaire.push(this.state.questions.filter(res => res.idQuestions === questions[i]))
-        // En restant dans le meme index que la question, on recherche dans notre base de données les choix ayant pour clés étrangères d'idQuestions
-        // le chiffre random et on copie le choix dans le tableau du questionnaire
-        Questionnaire[i].push(this.state.choices.filter(res => res.idQuestions === questions[i] ))
+      questions = [... new Set(questions)]
+      // Si le tableau de nombre uniques est égal à 80, on lance une boucle qui va remplir le questionnaire
+      if (questions.length == 80) {
+          for (let i=0; i< questions.length; i++) {
+              // On cherche l'id de la question correspondant au chiffre random dans notre base de données et on la copie dans le tableau du questionnaire
+              Questionnaire.push(this.state.questions.filter(res => res.idQuestions === questions[i]))
+              // En restant dans le meme index que la question, on recherche dans notre base de données les choix ayant pour clés étrangères d'idQuestions
+              // le chiffre random et on copie le choix dans le tableau du questionnaire
+              Questionnaire[i].push(this.state.choices.filter(res => res.idQuestions === questions[i] ))
+          }
+          // J'affiche le questionnaire pour vérifier la structure de celui-ci et je le copie dans sa state
+          this.setState({  questionnaire : Questionnaire, ready : true })
+          this.FirstStyleButtonChoices()
       }
-      // J'affiche le questionnaire pour vérifier la structure de celui-ci et je le copie dans sa state
-      console.log(" le questionnaire : ", Questionnaire)
-      this.setState({
-        questionnaire : Questionnaire, ready : true
-      })
-       this.FirstStyleButtonChoices()
-    }
-    else {
-      // Si le tableau de nombre unique n'atteint pas les 80 on relance le random pour le completer
-      // console.log("on relance")
-      this.RandomQuestionnaire()
-    }
-  } 
+      else {
+          // Si le tableau de nombre unique n'atteint pas les 80 on relance le random pour le completer
+          this.RandomQuestionnaire()
+      }
+    } 
 }
 
   StyleButtonChoices = () => {
-    const quest = Questionnaire.slice(this.state.previousQuestion,this.state.displayQuestion)
+    const quest = Questionnaire.slice(this.state.previousQuestion+1,this.state.displayQuestion+1)
    console.log("la question", quest)
-   console.log("nb points / question", this.state.pointQuestions)
+  //  console.log("nb points / question", this.state.pointQuestions)
     if (quest[0][0].Multiple === 0) {
       this.UniqueAnswer()
-       console.log("choix unique")
+      //  console.log("choix unique")
     }
     else if (quest[0][0].Multiple === 1) {
       this.MultipleAnswer()
-      console.log("choix multiple")
+      // console.log("choix multiple")
     }
   }
 
   FirstStyleButtonChoices = () => {
     const quest = Questionnaire[0]
-   console.log("la première question", quest[0].Multiple)
+  //  console.log("la première question", quest[0].Multiple)
     if (quest[0].Multiple === 0) {
       this.UniqueAnswer()
-      console.log("choix unique")
     }
     else if (quest[0].Multiple === 1) {
       this.MultipleAnswer()
-       console.log("choix multiple")
     }
-  }
-
-  NextQuestion = () => {
-    this.setState({ displayQuestion : this.state.displayQuestion+1 , previousQuestion: this.state.previousQuestion+1})
-    this.StyleButtonChoices()
-
   }
   
 
@@ -135,23 +123,92 @@ RandomQuestionnaire = () => {
 
 
   StockChoice = () => {
-    const tutu = this.state.choices.filter(choice => choice === choix)
-    console.log("tutu", tutu)
-    // this.setState({
-    //   choices_selected : 
-    // })
+    if (choixUnique !=0 ) {
+      //  console.log("je cherche ca",choixUnique)
+     let lechoix = this.state.choices.filter(choices => choices.idChoice == choixUnique.id)
+    //  console.log("lechoix", lechoix)
+       this.setState({
+          choices_selected : [...this.state.choices_selected, lechoix[0] ] 
+        })
+        // console.log('et je trouve ça', this.state.choices_selected)
+        // console.log("la value est ", lechoix[0].value)
+         if (lechoix[0].value == 1) {
+           score = score  + this.state.pointQuestions
+           console.log("mon score", score)
+           this.setState({
+             right_answers : this.state.right_answers +1
+           })
+         }
+         choixUnique=0
+    }
+    // Si le tableau des choix multiples à été rempli :
+    else if ( choixMultiple.length != 0) {
+      // on initie un tableau vide qui recevra l'objet entier des choix séléctionnés ; le nombre de réponses justes attendues; et un tableau comportant tous les choix
+      // possibles associés à la question
+      let leschoix = []
+      let reponsesattendues =0
+      let Allanswers = []
+      // console.log("le tableau des choix multiples", choixMultiple)
+      //Pour la  longueur du tableau des choix multiples
+      for (let i =0; i < choixMultiple.length; i++) {
+        // On filtre dans la liste des choix ceux qui ont été séléctionnés pour stocker l'objet entier
+         let hischoice = this.state.choices.filter(choices => choices.idChoice == choixMultiple[i])
+         leschoix.push(hischoice)
+         // on récupère l'id de la question associé au choix fait - le premier suffit -
+         let idquest = hischoice[0].idQuestions
+         // on récupère tous les choix associés à la question posée
+         Allanswers = this.state.choices.filter(choices => choices.idQuestions == idquest)
+         console.log("tous les choix possibles", Allanswers)
+         // Si la valeur d'un choix est egale a 1 c'est que la réponse est juste, on parcours à nouveau le tableau pour finir la boucle et stocker le nombre de réponses justes attendues
+         if (Allanswers[i].value === 1) {
+             reponsesattendues = reponsesattendues + 1
+        }
+         console.log("je veux", reponsesattendues, "réponses")
+         //on stock quoi qu'il arrive les choix faits par le candidat dans un tableau ( lui même dans un tableau ), pour l'affichage final
+        this.setState({
+          choices_selected : [...this.state.choices_selected, leschoix]
+        })
+        // console.log(" les choix faits sont", this.state.choices_selected)
+        let bonnesreponses = leschoix.filter(choix => choix[0].value == 1)
+        console.log("bonnes reponses", bonnesreponses.length)
+        // on va filtrer les choix a valeur true, et si le nombre est le même que le nombre de réponses attendues, on ajoute le nombre de points de la question
+        if ( bonnesreponses.length === reponsesattendues) {
+          score = score + this.state.pointQuestions
+          this.setState({
+            right_answers : this.state.right_answers +1
+          })
+        }
+        console.log("mon score est de :", score)
+      } choixMultiple =[]
+    }
+         this.setState({ displayQuestion : this.state.displayQuestion+1 , previousQuestion: this.state.previousQuestion+1})
+         this.StyleButtonChoices()
   }
 
   handleCheck = (e) => {
-    choix = ''
-     // const choix = document.getElementById(choices)
-     let id = e.target.id
-     let toto = e.target.value
-    console.log("c'est :", id)
-    console.log("la reponse est : ", toto) 
-    choix = id
+    // choixUnique =0
+     let unchoix = e.target
+     console.log("c'est ok ou pas ?",unchoix)
+    choixUnique = unchoix
   }
 
+  handleCheckMultipleAnswer =(e) => {
+    // console.log("les choix déjà là", choixMultiple)
+    let rosis = e.target.id
+    let rosarum = e.target.value
+    console.log("et c'essssstttt ", rosarum)
+    if (choixMultiple.includes(rosis)){
+      // console.log("c'est deja la", rosis)
+      let indexOfChoice = choixMultiple.indexOf(rosis)
+      // console.log('il ets en position', indexOfChoice)
+      choixMultiple.splice(indexOfChoice, 1)
+      // console.log("ducoup il est parti")
+    } else {
+      choixMultiple.push(rosis)
+      // console.log("ca y etait pas, on l'a mis !", rosis)
+    }
+    // console.log("la gueule du tableau", choixMultiple)
+  }
 
 componentDidMount =() => {
   this.GetDataTests()
@@ -173,35 +230,37 @@ componentDidMount =() => {
     return (
       <div>
         {ready === false ? <button onClick={this.RandomQuestionnaire}> Lancer le quizz </button> : <h3> Quizz Product Owner</h3> }
-        
-          <div className="Card-question">
-             <h2 className="number-question"> Question {displayQuestion}</h2>
+        <div class="main_container questions_container">
+            <div class="card question_card question_current">
+            <h1>Question {displayQuestion}</h1>
             {/* {ready ? this.GetAllChoices() : null } */}
             {ready ? questionnaire.slice(previousQuestion, displayQuestion).map(i =>
             
             <div key={i[0].idQuestions }>
-                 <p className="question">
-                {i[0].Question}
-              </p>
+              <p className="question"> {i[0].Question} </p>
+              <div class="checkbox_question">
+                <ul class="answer_list">
+              
               {multiple_answer ? 
               
               questionnaire.slice(previousQuestion, displayQuestion).map(i =>
               
               i[1].map(choices =>
                  <div>
-                 <input type="checkbox"  id={choices.idChoice} onChange={this.handleCheck} value={choices.answer} /> {choices.answer}
+                 <input type="checkbox"  id={choices.idChoice} value={choices.value} name={i[0].idQuestions} onChange={this.handleCheckMultipleAnswer} /> {choices.answer}
               </div>))
                 :
 
                 questionnaire.slice(previousQuestion, displayQuestion).map(i =>
                   i[1].map(choices =>
                 <div>
-                <input type="radio" id={choices.idChoice} value={choices.answer} name={i[0].idQuestions}  onChange={this.handleCheck} /> {choices.answer}
+                <input type="radio" id={choices.idChoice} value={choices.value} name={i[0].idQuestions}  onChange={this.handleCheck} /> {choices.answer}
                 
                 </div>)
                 )
                }
-
+              </ul>
+              </div>
             </div>)
               : null}
 
@@ -219,10 +278,10 @@ componentDidMount =() => {
                 </div>) }  
  */}
 
-              <button onClick={this.NextQuestion} className="Next"> Suivant</button>
+              <button onClick={this.StockChoice} className="Next"> Suivant</button>
           </div>
           <h2> {displayQuestion} / 80</h2>
-    
+        </div>
       </div>
     )
   }
